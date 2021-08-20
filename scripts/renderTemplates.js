7be66e2fb916952ea658f9963ca725433ac7afbc
Pavel Pomerantsev <pomerantsevp@gmail.com>
2021-08-20 19:55:32 +0300

    Start rendering templates with nunjucks

---
+++ scripts/renderTemplates.js
@@ -0,0 +1,11 @@
+const fs = require('fs/promises');
+const nunjucks = require('nunjucks');
+
+const renderTemplate = async (fileName) => {
+  await fs.writeFile(`public/${fileName}`, nunjucks.render(`src/${fileName}`));
+};
+
+Promise.all([
+  renderTemplate('index.html'),
+  renderTemplate('404.html'),
+]);
